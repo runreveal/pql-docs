94302ec81095873cdaff333cc31f3e87a81ae08d
Evan Johnson <evan@runreveal.com>
2024-03-10 15:35:42 -0500

    pql docs site.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span className="font-mono">pql | </span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/runreveal/pql',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/NZS9QtCJXt',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/runreveal/pql-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'pql is a project by runreveal, copyright 2024',
   },
 }
 
